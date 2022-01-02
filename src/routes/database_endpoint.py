f3e0288f6e0fcc5e1faa7ece400e86f88ca37542
yanover <y-schoeni@hotmail.com>
2022-01-02 20:09:08 +0100

    Add class wrapper for mysql connections

--- src/routes/database_endpoint.py
+++ src/routes/database_endpoint.py
@@ -1,23 +1,30 @@
+import os
 from flask import Blueprint, request, abort, current_app
-from models.Stack import Stack
-from PyMySQL import _mysql
+from models.mysql_handler import MYSQL
+from MySQLdb import _mysql
 
 # Declare Blueprint
 database_endpoint = Blueprint("database_endpoint", __name__)
 
 # Route default path
-ENDPOINT = '/mysql'
+ENDPOINT = "/mysql"
 
-# In memory Pile
-stack = Stack()
-
-
-@database_endpoint.route(ENDPOINT, methods=['GET'])
+@database_endpoint.route(ENDPOINT, methods=["GET"])
 def default():
-    current_app.logger.debug('Retrieve backups')
-    return str(stack.peek())
+    current_app.logger.debug("Retrieve existing databases")
+    pass
 
 
-@database_endpoint.route(f"{ENDPOINT}/backup", methods=['GET'])
-def min():
-    return str(stack.min())
+@database_endpoint.route(f"{ENDPOINT}/backup", methods=["GET"])
+def backup():
+
+    # Load configurations
+    HOST = os.environ.get("MYSQL_HOST")
+    PORT = int(os.environ.get("MYSQL_PORT"))
+    USER = os.environ.get("MYSQL_USER")
+    PASS = os.environ.get("MYSQL_PASS")
+
+    database = MYSQL(HOST, PORT, USER, PASS)
+    handler = database.connect()
+    print(handler.query("SHOW DATABASES;"))
+    return ""
